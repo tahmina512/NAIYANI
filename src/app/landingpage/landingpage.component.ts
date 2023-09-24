f48261dceb053e2d91ffcb5d13f153283fed8473
tish001 <tahminatish001@gmail.com>
2023-09-24 16:58:48 +0600

    transition edit

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -10,7 +10,7 @@ export class LandingpageComponent implements OnInit {
   moveLogo: boolean = false;
   typedText: string = '';
   showShape: boolean = false;
-  
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {
@@ -25,22 +25,20 @@ export class LandingpageComponent implements OnInit {
         this.typedText = content.slice(0, index);
         index++;
         setTimeout(type, 200);
-      }
-      else {
-      
+      } else {
         setTimeout(() => {
           this.showShape = true; // Show the shape
 
-          // setTimeout(() => {
-          //   this.moveLogo = true; // Trigger the move-up animation
-          //   setTimeout(() => {
-          //     this.router.navigate(['/signin']);
-          //   }, 3000);
-          // }, 2000); // Adjust the delay as needed for the brief moment
-        }, 700);
+          setTimeout(() => {
+            this.moveLogo = true; // Trigger the move-up animation
+            setTimeout(() => {
+              this.router.navigate(['/signin']);
+            }, 3000);
+          }, 4000); // Adjust the delay as needed for the brief moment
+        }, 500);
       }
     };
 
-    setTimeout(type, 10); // Delay before typing starts 
+    setTimeout(type, 10); // Delay before typing starts
   }
 }
